2495ce6c8fda774c29911952641ee6439a28fdd7
kirubel <kirubel@gmail.com>
2022-04-08 14:07:50 -0700

    this is for posibel conbination of two digit num

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main -printes all posibel conbination of two digit numbers
+ *
+ * return alwyes zero
+ */
+int main(void)
+{	
+int num1, num2;
+for (num1=0; num1 <=98; num++)
+{
+for (um2 = num1 +1; num2 <=99; num2++)
+{	
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+
+if (num1 == 98 && num2 == 99)
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
