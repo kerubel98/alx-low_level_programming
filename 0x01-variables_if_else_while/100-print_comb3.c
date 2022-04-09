89d01e4d5de98b1d05dec383db53acd2f0bad89c
kirubel <kirubel@gmail.com>
2022-04-08 17:57:19 -0700

    m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,17 +10,17 @@ int main(void)
 {	
 	int i, j;
 
-	for(i = 0; i < 9; i++)
+	for (i = 0; i < 9; i++)
 	{
 		for ( j = i + 1; j < 8; j++)
 		{ 
 		putchar((i % 10) + '0');
 		putchar((j % 10) + '0');
-	if (i == 8 && j == 9)
+	        if (i == 8 && j == 9)
 		continue;
 		
-		putchar(',');
-		putchar(' ');
+	        putchar(',');
+	        putchar(' ');
 		}
 	}
 	
