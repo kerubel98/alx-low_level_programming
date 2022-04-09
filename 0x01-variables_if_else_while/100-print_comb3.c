8bce508b86c1aea7584d364ae0e306fe31fe682c
kirubel <kirubel@gmail.com>
2022-04-08 17:48:54 -0700

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,51 +1,30 @@
-include<stdio.h>
+#include <stdio.h>
 
 /**
- * main- the progeram startes hear
+ * main- where the progera tartes
  *
  *
- * Return: alweyes zero
+ * Return: alwaye zero
  */
 int main(void)
-{
-int i, j;
+{	
+	int i, j;
 
-for (i = 0; i < 9; i++)
-{
-for (j = i + 1; j < 10; j++)
-{
-putchar((i % 10) + '0');
-putchar((j % 10) + '0');
+	for(i = 0; i < 9; i++)
+	{
+		for ( j = i + 1; j < 8; j++)
+		{ 
+		putchar((i % 10) + '0');
+		putchar((j % 10) + '0');
+	if (i == 8 && j == 9)
+		continue;
+		
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	
+	putcahr('\n');
 
-
-if (i == 8 && j == 9)
-continue;
-
-putchar(',');
-putchar(' ');
-}
-}
-
-putchar('\n');
-
-return (0);
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                                                                                                                                                              25,1          Al
+		return (0);
+}	
