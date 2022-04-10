15e5702b9e28326b1a5d4f5ac3f117434cb88abf
kirubel <kirubel@gmail.com>
2022-04-10 15:11:08 -0700

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - is intery point
+ *
+ *
+ * Return: is zero
+ */
+int main(void)
+{
+	int i;
+	 for (i = 0; i < 10; i++)
+		 purchar('i');
+	 putchar('\n');
+
+	 return (0);
+}
