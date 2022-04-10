5c7c2ae0184215f073584ac5dc2c91895e10cb6b
kirubel <kirubel@gmail.com>
2022-04-10 14:20:53 -0700

    m

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int c;
 
-	 for (c = 0; c < 26; ++c)
+	for (c = 0; c < 26; ++c)
 		 putchar('a' + c);
 
-	 putchar('\n');
+	         putchar('\n');
 
-	 return (0);
+	return (0);
 
 }
