8bbd0a87c50d5d9aeb69e4f3dc2eb4484f293952
kirubel <kirubel@gmail.com>
2022-04-11 06:33:01 -0700

    m

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *
+ * main -the entry point 
+ *
+ */
+int main(void
+	char c;
+
+	for (c = 0; c < 26; c++)
+
+		putchar('a' + c);
+
+	for (c = 0; c < 26; c++)
+		putchar('A' + c);
+
+	putchar('\n');
+	
+	return (0);
+}
