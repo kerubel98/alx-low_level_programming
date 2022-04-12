568a28fede000540125e57846c0da54643e41788
kirubel <kirubel@gmail.com>
2022-04-12 13:54:43 -0700

    m

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_abs -returns absulut valu of a 
+ *_abs -returns absulut valu of a
  *@a: output changes the value of a
  *Return: the systm retuns postive valu of a
  */
