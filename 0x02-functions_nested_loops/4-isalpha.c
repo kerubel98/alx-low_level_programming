3619c4acfc6ea7ee2d456b9c0d45fc64ca5245f4
kirubel <kirubel@gmail.com>
2022-04-12 12:40:33 -0700

    m

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,15 @@
 
 /**
  *_isalpha -chacke wather it is no or char
- * @c is the return type 
+ * @c: is the return type
+ *
  * Return: int is the return type
  */
 int _isalpha(int c)
-{ 
+{
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
-	else 
+	else
 		return (0);
 }
-	       	
+
