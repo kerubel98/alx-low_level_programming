cce0f3ec40bd33b616089e2f1d735db90dcda276
kirubel <kirubel@gmail.com>
2022-04-12 12:37:08 -0700

    m

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isalpha -chacke wather it is no or char
+ * @c is the return type 
+ * Return: int is the return type
+ */
+int _isalpha(int c)
+{ 
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else 
+		return (0);
+}
+	       	
