bca1404369ee3a560aad2bad5478821570afddbc
kirubel <kirubel@gmail.com>
2022-04-12 07:38:24 -0700

    m

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - is wher the progeram starts
+ *
+ *
+ * Return: valus are what the function returns 
+ */
+int main(void)
+{
+	char c = void print();
+
+	return (0);
+}
+void print()
+{
+	putchar("_putchar\n")
+		return (0);
+}
