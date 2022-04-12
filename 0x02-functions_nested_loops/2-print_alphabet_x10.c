3d3ab6d4371f0a05e18b09131d8e70302bbce8ae
kirubel <kirubel@gmail.com>
2022-04-12 11:08:58 -0700

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10(void) - print alphabet
+ *
+ */
+void print_alphabet_x10(void);
+{
+	char c;
+	int a;
+
+	while(a <= 10)
+	{
+	for (c = 'a'; c = 'z'; c++)
+		_putchar(a);
+	a++
+	}
+}
+
