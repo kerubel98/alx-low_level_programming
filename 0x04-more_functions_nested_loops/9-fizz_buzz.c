b482511b3eef0f1866ccb867daa08812b8acafc3
kirubel <kirubel@gmail.com>
2022-04-15 16:35:07 -0700

    m

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,30 +8,30 @@
 
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if ( % 3 == 0 && i % 5 != 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf("buzz");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else
-		{
-			printf("%d", i);
-		}
-		if ( i != 100)
-			putchar(' ');
-		else
-			putchar('\n');
-	}
-	return (0);
+for (i = 1; i <= 100; i++)
+{
+if ( % 3 == 0 && i % 5 != 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf("buzz");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else
+{
+printf("%d", i);
+}
+if ( i != 100)
+putchar(' ');
+else
+putchar('\n');
+}
+return (0);
 }
