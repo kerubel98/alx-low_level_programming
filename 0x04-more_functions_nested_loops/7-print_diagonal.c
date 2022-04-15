48bd0239f0453d8255b73428d5624f9383113150
kirubel <kirubel@gmail.com>
2022-04-15 14:57:53 -0700

    m

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,16 +14,17 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-	for (j = 0; J < i; j++)
+	for (j = 0; j < i; j++)
 	{
 	_putchar(' ');
 	}
 	_putchar('\\');
 	_putchar('\n');
 	}
+	}
 	else
 	{
 	_putchar('\n');
 	}
-	}
+
 }
