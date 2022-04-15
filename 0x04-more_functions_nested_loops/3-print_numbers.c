653cb9ce276026f9b740a26bd89616dc61b479d4
kirubel <kirubel@gmail.com>
2022-04-15 12:51:03 -0700

    mm

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,6 +7,7 @@
  */
 void print_numbers(void)
 {
+	
 	int i = 0;
 
 	for (i = 0; i < 10; i++)
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,18 @@
  */
 void print_most_numbers(void)
 { 
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	for (i = 0; i < 10; i++)
 	{
 
+
 	if (i == 2 || i == 4)
 	
 		i++;
+
 		_putchar(i + '0');
 	}
 	_putchar('\n');
