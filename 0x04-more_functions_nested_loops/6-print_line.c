a89ee4b83e6ba63497410176a0b750072cf6f36f
kirubel <kirubel@gmail.com>
2022-04-15 14:19:11 -0700

    m

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - printes line 
+ * @n: is the lengeth of the line
+ * 
+ * Return: no return valiue for void
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+	for (i = 0; i <= n; i++)
+	{
+	_putchar('_')
+	}
+	}
+	_putchar('\n');
+}
