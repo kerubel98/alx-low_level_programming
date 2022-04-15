426feca9e5c5bf6c8ef47207703a6f75723b0e75
kirubel <kirubel@gmail.com>
2022-04-15 13:23:24 -0700

    m

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_numbers - 
+ * print_numbers - print th number zero to nine
  *
- *
+ * Return: alwyes zero
  */
 void print_numbers(void)
 {
-	
+
 	int i = 0;
 
 	for (i = 0; i < 10; i++)
