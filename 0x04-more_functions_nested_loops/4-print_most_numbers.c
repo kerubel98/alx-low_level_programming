de38576b1e980e294904afd28c8ed5de23a6928e
kirubel <kirubel@gmail.com>
2022-04-15 12:31:39 -0700

    m

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - 
+ *
+ *
+ */
+void print_most_numbers(void)
+{ 
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+
+	if (i == 2 || i == 4)
+	
+		i++;
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
