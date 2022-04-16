c6b78a75de6d0161e56dee198f8cd07488e7c1bb
kirubel <kirubel@gmail.com>
2022-04-15 17:22:11 -0700

    m

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,11 +13,13 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-		for (j = 0; j < i; j++)
+		for (j = 0; j < size; j++)
 		{
+		if ((size - i -1) > j)
 		_putchar(' ');
-		}
+		else
 		_putchar('#');
+		}
 		_putchar('\n');
 		}
 	}
