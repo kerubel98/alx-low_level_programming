a83e1432461f7b218370b0a523fcd2eef11c4456
kirubel <kirubel@gmail.com>
2022-04-16 05:19:31 -0700

    m

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ *
+ * Reurn: zero
+ */
+
+int main(void)
+{
+	unsigned long n, i, max;
+
+	n = 612852475143;
+	max = 1;
+
+	for (i = 3; i <= n; i += 2)
+	{
+		while (n % i == 0)
+		{
+			max =n;
+			n = n /i;
+		}
+	}
+	printf("%lu\n", max);
+	return (0);
+}
