a731f8e685c0ad25e558e3dbfcfc0f132f5faf77
kirubel <kirubel@gmail.com>
2022-04-18 12:22:27 -0700

    task 02

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - takes sting
+ * @s - pointes to sring
+ * Return : length of string
+ */
+
+int _strlen(char *s)
+{
+	char lngth:
+
+	lngth = strlein(*s);
+
+	return (lngth);
+}
