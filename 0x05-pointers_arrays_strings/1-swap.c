560be7090c4593e7adfb74109a4a36274145f837
kirubel <kirubel@gmail.com>
2022-04-18 12:00:10 -0700

    m

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,14 +3,15 @@
 /**
  * swap_int - the function swaps n to b
  *
- * @*a: is pointer
- * @*b: is pointer
+ * @a: is pointer
+ * @b: is pointer
  * Return : void
  */
 
 void swap_int(int *a, int *b)
 {
 	int t;
+
 	t = *a;
 	*a = *b;
 	*b = t;
