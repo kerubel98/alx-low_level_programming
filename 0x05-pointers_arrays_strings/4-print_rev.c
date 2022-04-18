85e24ce798d7d528363017abf9987e3d873cc3f3
kirubel <kirubel@gmail.com>
2022-04-18 14:40:42 -0700

    m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,12 @@
 
 void print_rev(char *s)
 {	
-	int i, len, temp;
+	int i, len;
 	len = strlen(s);
 
 	for ( i = len; i < 0; i++)
 	{
-		temp = s[i];
+		
 		s[i] = s[len - i - 1];
 		printf("%s\n", s);
 	}
