150da313d7befd7a4ae4ecf68224390ce78f2336
kirubel <kirubel@gmail.com>
2022-04-19 13:09:24 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints array
+ * @a: arey
+ * @n: arrey
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int len = 0;
+
+	while (len <= n -1)
+	{
+		printf("%d", a[len]);
+		if (len != n-1)
+		{
+			printf(",");
+		}
+		len++;
+	}
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void print_rev(char *s);
 int _putchar(char c);
 void randompas(int N);
 char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
 
 #endif
