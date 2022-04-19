fb038d65a04522293448355d586c3e808bc4f920
kirubel <kirubel@gmail.com>
2022-04-19 09:13:42 -0700

    task 05

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,3 +9,21 @@
 
 void rev_string(char *s)
 {
+	int len = 0;
+	int len2 = 0;
+	char tmp;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	len = len - 1;
+	while (len2 <= len)
+	{
+		tmp = s[len2];
+		s[len2] = s[len];
+		s[len] = tmp;
+		len2++;
+		len--;
+	}
+}
