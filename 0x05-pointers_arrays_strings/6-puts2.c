dc72896aa429c7fdc2a3ad158f7a9363d575c6b7
kirubel <kirubel@gmail.com>
2022-04-19 10:32:09 -0700

    m

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts2 - printe the next num 
+ * 
+ * @str: parameter
+ * Returen: void
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	int len2 = 0;
+	char tmp;
+
+	while (str[len] != 0)
+	{	
+		len++;
+	}
+	len = len - 1;
+	while (len2 <= len)
+ 	{
+		tmp = str[len2];
+		str[len2] = str[len];
+		str[len] = tmp;
+		len2++;
+		len--;
+	}
+}
