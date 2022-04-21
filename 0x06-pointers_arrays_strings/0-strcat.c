78e189425bc706eda621a95a7af04ac0fb46371c
kirubel <kirubel@gmail.com>
2022-04-21 12:00:26 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat -concatenate two stringes
+ *
+ * @dest: the firt string
+ * @src: the scond string
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int j = 0;
+	int i;
+
+	for (i = 0; i = '\0'; i++)
+	{
+		dest [j] += src [i];
+		j++;
+	}
+	return (dest);
+}
