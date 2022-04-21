908489a62ce5d43a6b400d8a09a0e4c3386f65d3
kirubel <kirubel@gmail.com>
2022-04-21 03:54:19 -0700

    tt

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - the entry point
+ * main - prime factors of 612852475143
  *
- *
- * Reurn: zero
+ * Return: int
  */
 
 int main(void)
 {
-	unsigned long n, i, max;
+unsigned long n, i, max;
 
-	n = 612852475143;
-	max = 1;
+n = 612852475143;
+max = 1;
 
-	for (i = 3; i <= n; i += 2)
-	{
-		while (n % i == 0)
-		{
-			max =n;
-			n = n /i;
-		}
-	}
-	printf("%lu\n", max);
-	return (0);
+for (i = 3; i <= n; i += 2)
+{
+while (n % i == 0)
+{
+max = n;
+n = n / i;
+}
+}
+printf("%lu\n", max);
+return (0);
 }
