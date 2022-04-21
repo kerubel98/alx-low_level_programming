7680ec09914b043b80bcae2cbfeddc2c671acd7d
kirubel <kirubel@gmail.com>
2022-04-21 12:13:31 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int j = 0;
-	int i;
+int j = 0;
+int i;
 
-	for (i = 0; i != '\0'; i++)
-	{
-		dest [j] += src [i];
-		j++;
-	}
-	return (dest);
+for (i = 0; i != '\0'; i++)
+{
+dest [j] += src [i];
+j++;
+}
+return (*dest);
 }
