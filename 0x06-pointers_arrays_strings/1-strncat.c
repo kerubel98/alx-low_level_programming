9407a1f57eb2b1d649027dfc4906f2a8c11c58e4
kirubel <kirubel@gmail.com>
2022-04-21 13:23:05 -0700

    task 01

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y;
+	for (x = 0; dest[x] != '\0'; x++)
+		;
+	for (y = 0; src[x] != '\0' && n == 0; y++)
+	{
+		dest[x] = src[y];
+		x++;
+		n--;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
