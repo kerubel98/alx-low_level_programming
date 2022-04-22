61d5cd0243e630314eecc927092df52ad25a743a
kirubel <kirubel@gmail.com>
2022-04-22 07:38:34 -0700

    task 03

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _strcmp - compaer to stringes 
+ * @s1: the first string
+ * @s2: the second string
+ * Return: zero
  *
  */
 
@@ -12,22 +12,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int x;
 
-	for ( x = 0; s1[x] != 0 && s1[x] == s2[x]; x++)
+	for ( x = 0; s1[x] != 0 || s2[x] != '\0'; x++)
 	{
-		if (s1[x] < s2[x])
-		{
-		
-			return (-1);
-		}
-		else if (s1[x] > s2[x])
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-		return (0);
+		if (s1[x] != s2[x])
+			return (s1[x] - s2[x]);
 	}
+	return (0);
 }
 
