eb6f15b96c1ebc7a8b4c18468fb39cf239f4484e
kirubel <kirubel@gmail.com>
2022-04-22 10:52:07 -0700

    task 04

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,10 +13,10 @@ void reverse_array(int *a, int n)
 {
 	int x, y;
 
-	for (x = 0; x < (n / 2); x--)
+	for (x = 0; x < (n / 2); x++)
 	{
-		y =a[x];
-		a[x] = a[n-x-1];
+		y = a[x];
+		a[x] = a[n - x - 1];
 		a[n - x - 1] = y;
 	}
 }
