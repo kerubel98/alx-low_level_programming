622e72f10aa35e329faba0411722f99a60469a8c
kirubel <kirubel@gmail.com>
2022-04-22 16:39:20 -0700

    task 07

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,13 +14,13 @@ char *leet(char *s)
 	int y;
 
 	char cod[] = "aeotl";
-	char ucod[] = "AEOTL"
+	char ucod[] = "AEOTL";
 	char num[] = "43071";
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
 
-		for ( y = 0; cod [x] != '\0' && ucod[b] != '\0'; y++)
+		for ( y = 0; cod [x] != '\0' && ucod[y] != '\0'; y++)
 		{
 			if ( s[x] == cod[y] || s[x] == ucod[y])
 			{
