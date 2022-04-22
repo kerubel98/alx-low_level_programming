6a91e7a166c25f252a48f1790e1d6537ae0ab607
kirubel <kirubel@gmail.com>
2022-04-21 17:04:40 -0700

    task 09

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -14,14 +14,9 @@ int main(void)
 
 	a[2]  = 1024;
 	p = &n;
-	/*
-	 *
-	 *
-	 *
-	 *
-	 *
-	 */
+	
 	*(p + 5) = 98;
 	/* --o thst this printes 98\n-*/
 	 printf("a[2] = %d\n", a[2]);
 	 return (0);
+}
