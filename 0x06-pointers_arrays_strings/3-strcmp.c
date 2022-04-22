597cb1151726d6f3f88ffb7370db1cccae4f5b95
kirubel <kirubel@gmail.com>
2022-04-22 07:15:21 -0700

    task 02

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,14 +12,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int x;
 
-	for ( x = 0; s1[x] != 0 && s1[x] == s2[y]; x++)
+	for ( x = 0; s1[x] != 0 && s1[x] == s2[x]; x++)
 	{
-		if (s1[i] < s2[i])
+		if (s1[x] < s2[x])
 		{
 		
 			return (-1);
 		}
-		else if (s1[i] > s2[i])
+		else if (s1[x] > s2[x])
 		{
 			return (1);
 		}
