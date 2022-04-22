cd40f4d49106dab394ce7db83a20ecb5cb890815
kirubel <kirubel@gmail.com>
2022-04-22 07:12:01 -0700

    tak 03

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+
+	for ( x = 0; s1[x] != 0 && s1[x] == s2[y]; x++)
+	{
+		if (s1[i] < s2[i])
+		{
+		
+			return (-1);
+		}
+		else if (s1[i] > s2[i])
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+		return (0);
+	}
+}
+
