36deda14b64567f8f4109334ad380a066f9f0d72
kirubel <kirubel@gmail.com>
2022-04-22 17:22:59 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,13 @@
-#include <stdio.h>
+#include "main.h"
 
-/**
+/*
  *
  *
  *
  *
  */
-int main(void)
+
+void print_number(int n)
 {
-        int n;
-        int a[5];
-        int *p;
-
-        a[2]  = 1024;
-        p = &n;
-
-        *(p + 5) = 98;
-        /* --o thst this printes 98\n-*/
-         printf("a[2] = %d\n", a[2]);
-         return (0);
+	_putchar("%d\n", n);
 }
-
