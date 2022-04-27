cba9acb053381a6c2382253a03769d708fc14fe5
kirubel <kirubel@gmail.com>
2022-04-27 04:36:50 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ puts_recursion - prints a string, followed by a new line
+ *
+ * @s: The string that prints
+ *
+ * Return: nothing*
+ */
+void _puts_recursion(char *s)
+{
+
+	_putchar (*s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_puts_recursion(s + 1);
+}
