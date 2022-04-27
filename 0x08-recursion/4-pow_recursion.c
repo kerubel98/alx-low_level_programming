f65e553c03dca32087229d278d61815e27cbf3ed
kirubel <kirubel@gmail.com>
2022-04-27 06:22:37 -0700

    task 05

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,7 +7,7 @@
  * @y: the power to raise @n to
  *
  * Return: the value of @x to the @y power
- * 
+ *
  */
 int _pow_recursion(int x, int y)
 {
@@ -16,5 +16,5 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 		return (1);
 
-	return (x * _pow_recursion(x, y-1));
+	return (x * _pow_recursion(x, y - 1));
 }
