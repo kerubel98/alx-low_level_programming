b9f51f5b864965d2cb316f416480caeb0b8bc44d
kirubel <kirubel@gmail.com>
2022-04-29 07:02:08 -0700

    task 04

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,16 +13,28 @@
 
 int main(int argc, char* argv[])
 {
-	int i, sum = 0;
-
-	if(argc > 1)
+	int c, d, n = 0;
+	
+	if (argc < 2)
 	{
-		for(i = 1; i < argc; i++)
-		{
-			sum += atoi(argv[i]);
-		}
-
-		printf("Total = %d\n", sum);
+		printf("0\n");
+		return (0);
 	}
-	return 0;
+
+	for (c = 1; c < argc; c++)
+	{
+		for (d = 0; argv[c][d] != '\0'; d++)
+		{
+			if (argv[c][d] < '0' || argv[c][d] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		n += atoi(argv[c]);
+
+	}
+	printf("%d\n", n);
+	return (0);
+
 }
