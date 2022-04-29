2dcc7bfea03a4d424e8028b9028abe0af199e8ea
kirubel <kirubel@gmail.com>
2022-04-29 04:22:13 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - is the main mathod 
+ * @argc: is argument count argc passed 
+ * 
+ * @argv: argument vector 
+ */
+
+int main (int argc, char *argv[])
+{
+	(void)argc;
+	
+	printf("%s\n", argv[0]);
+
+	return (0);
+
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+this repo is to show how to print file name using command linr arguments in c
+
