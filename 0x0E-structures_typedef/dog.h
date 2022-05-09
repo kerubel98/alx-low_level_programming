bc98f7b323f23c91a28e8f80ed968272a911e502
kirubel <kirubel@gmail.com>
2022-05-09 11:10:49 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+/**
+ * struct dog - describes the sweet boy or girl
+ * @name: name of dog
+ * @age: how old is that dog?
+ * @owner: who is the lucky person that gets to live with that dog?
+ */
 struct dog 
 {
 	char *name;
 	float age;
-	char owner;
+	char *owner;
 };
+
 #endif
