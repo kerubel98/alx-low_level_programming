2072f8719972613eb71e68d4dd67de1e781d843a
kirubel <kirubel@gmail.com>
2022-05-09 12:50:04 -0700

    task 04

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,5 +22,7 @@ void print_dog(struct dog *d);
  * typedef dog_t - define a new name for type struct dog
  */
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strdup(char *str);
 
 #endif
