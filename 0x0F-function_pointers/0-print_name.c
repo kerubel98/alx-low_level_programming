3f3b9bceb568607ff8df0ba2a7abc099fdaee88b
kirubel <kirubel@gmail.com>
2022-05-11 10:50:48 -0700

    tasko

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,10 @@
 #include "function_pointers.h"
 
 /**
- *
- *
- *
- *
+ *print_name - prints a name
+ * @name: the name to print
+ * @f: the function to send the name to
+ * Return: nothing
  *
  */
 
@@ -13,5 +13,6 @@ void print_name(char *name, void (*f)(char *))
 {
 	if(!name || !f);
 		return;
-	f(name);	
+	f(name);
+
 }
