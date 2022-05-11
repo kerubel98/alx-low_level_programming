f899578ffee6770df4bb203e66735e9372ad9809
kirubel <kirubel@gmail.com>
2022-05-11 12:59:25 -0700

    task 01

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - performes whatver action performes on array
+ * @array: is parrameter to be passed to action
+ * @size: tayp size_t parameter
+ * @action: is function to be performed 
+ * @Return: void 
+ *
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+	void (*p)(int);
+
+	if (!array || !action)
+		return;
+	for(i = 0; i < size; i++)
+		p(array[i]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,7 @@
 #define _FUNCTION_POINTERS_H_
 void print_name(char *name, void (*f)(char *));
 void print (char*);
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 
 #endif 
