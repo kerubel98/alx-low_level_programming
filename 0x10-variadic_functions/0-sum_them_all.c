c215a61c46b8b67745eb60fa37897441663a28c4
kirubel <kirubel@gmail.com>
2022-05-13 04:53:38 -0700

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sumantion of var arg
+ * @n: number of ardumentes pased
+ * @... :elpsis sign that indecate the prog take varabel num of arguments
+ * Return: value
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list arg;
+	int value = 0;
+
+	va_start(arg, n);
+
+	for (int i = 0; i < n; i++)
+	{
+		value += va_arg(arg, int);
+	}
+	return (value);
+
+}
---
+++ 0x10-variadic_functions/main.h
@@ -0,0 +1,5 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+int sum_them_all(const unsigned int n, ...);
+
+#endif 
