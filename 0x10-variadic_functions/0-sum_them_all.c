7824da39b2f9fa89c54091336882eaaf7933339e
kirubel <kirubel@gmail.com>
2022-05-13 05:47:41 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,7 +9,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list arg;
-	int value = 0, i;
+	unsigned int value = 0, i;
 
 	if (n == 0)
 		return (0);
@@ -17,9 +17,9 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(arg, n);
 
 	for (i = 0; i < n; i++)
-	
+
 		value += va_arg(arg, int);
-	
+
 	va_end(arg);
 
 	return (value);
