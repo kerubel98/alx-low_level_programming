e83c934dda3ac280d7115265e1e21e5f749be82c
kirubel <kirubel@gmail.com>
2022-05-13 06:58:27 -0700

    task 0

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(arg, n);
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		or = (va_arg(arg, char*));
 		if (or != NULL)
