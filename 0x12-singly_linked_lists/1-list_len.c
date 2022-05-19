2492411097a99fd5951e7fb5b422070c6800e68b
kirubel <kirubel@gmail.com>
2022-05-19 11:44:51 -0700

    task 0

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
