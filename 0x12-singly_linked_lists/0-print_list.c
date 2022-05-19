886677e83ebc88ea0b5698eeb8c14de61a008dda
kirubel <kirubel@gmail.com>
2022-05-19 11:15:14 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,8 +9,15 @@
 
 size_t print_list(const list_t *h)
 {
-    int i = 0;
-   const list_t  *current_node = h;
+    int i =0;
+
+    const list_t  *current_node = h;
+    current_node = malloc(sizeof(list_t));
+    if (current_node = NULL)
+    {
+	    printf("p[0] (nill)");
+    }
+
     while (current_node != NULL)
     {
         printf("%d", current_node ->data);
