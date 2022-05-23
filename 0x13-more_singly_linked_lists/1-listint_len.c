4d55f78465be022b48131251e8f1e91a83a53db7
kirubel <kirubel@gmail.com>
2022-05-23 06:28:40 -0700

    task-02

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements in a list
+ * @h: the list to check
+ * Return: the number of elements in the lists
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -15,4 +16,6 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif
