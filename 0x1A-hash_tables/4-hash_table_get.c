05793e4d13bcd2062932fec4cb6d9cb2b009c2a0
kerubel98 <kirubeltadesse98@gmail.com>
2023-12-24 11:24:58 -0800

    fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,38 +1,34 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - reteives a value associated with key
- * @ht: hash table
- * @key: key to look for
- *
- * Return: value at key
- *
- */
+ *  * hash_table_get - reteives a value associated with key
+ *   * @ht: hash table
+ *    * @key: key to look for
+ *     *
+ *      * Return: value at key
+ *       *
+ *        */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int indx;
+	unsigned long int idx;
 	hash_node_t *node;
 
 	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (NULL);
 
-	indx = key_index((unsigned char *)key, ht->size);
+	idx = key_index((unsigned char *)key, ht->size);
 
-	if (ht->array[indx] == NULL || ht->array[indx] == 0)
+	if (ht->array[idx] == NULL || ht->array[idx] == 0)
 		return (NULL);
-	if (strcmp(ht->array[indx]->key, key) == 0)
-	{
-		return (ht->array[indx]->value);
 
-		node = ht->array[indx];
-		while (node != NULL)
-		{
-			if (strcmp(node->key, key) == 0)
-			{
-				return (node->value);
-			}
-			node = node->next;
-		}
+	if (strcmp(ht->array[idx]->key, key) == 0)
+		return (ht->array[idx]->value);
+	node = ht->array[idx];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
 	}
 	return (NULL);
 }
